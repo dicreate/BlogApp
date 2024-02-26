0d83f165aa1c374c81d0c6488b9a07aa347cbad4
DmitriyKovchugo <dkovchugo@mail.ru>
2024-02-26 14:25:42 +0300

    feat: create pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { About, Dashboard, Home, Projects, SignIn, SignUp } from "./pages"
 function App() {
 
   return (
-    <>
-      123
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
