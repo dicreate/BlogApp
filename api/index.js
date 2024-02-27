811467de0a7eccd2d67e80b62626bcf43ff40310
DmitriyKovchugo <dkovchugo@mail.ru>
2024-02-27 10:47:40 +0300

    feat: add signup functionality

--- api/index.js
+++ api/index.js
@@ -1,16 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
+// connected DB
 dotenv.config();
-
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("mongodb is connected"))
   .catch((e) => console.log(e));
 
+// RUN express server
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
