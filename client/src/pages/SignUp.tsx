695ba24dc1de8e2cd2d5773c6ded7965b35baed7
DmitriyKovchugo <dkovchugo@mail.ru>
2024-02-27 23:03:51 +0300

    feat: add functionality to sign-up

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,7 +1,51 @@
-import { Button, Label, TextInput } from "flowbite-react"
-import { Link } from "react-router-dom"
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react"
+import { Link, useNavigate } from "react-router-dom"
+import { ChangeEvent, FormEvent, useState } from "react"
 
+interface Iform {
+   username?: string,
+   email?: string,
+   password?: string
+}
 const SignUp = () => {
+   const [formData, setFormData] = useState<Iform>({});
+   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+   const [loading, setLoading] = useState<boolean>(false);
+   const navigate = useNavigate();
+
+   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+      setFormData({ ...formData, [event.target.id]: event.target.value.trim() });
+   }
+
+   const handleSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+      e.preventDefault();
+      if (!formData.username || !formData.email || !formData.password) {
+         return setErrorMessage("Please fill out all fields")
+      }
+      try {
+         setLoading(true);
+         setErrorMessage(null);
+         const res = await fetch('/api/auth/signup', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
+         });
+         const data = await res.json();
+         if (data.success === false) {
+            return setErrorMessage(data.message)
+         }
+         if (res.ok) {
+            navigate('/sign-in');
+         }
+      } catch (error) {
+         if (error instanceof Error) {
+            setErrorMessage(error.message)
+         }
+      } finally {
+         setLoading(false);
+      }
+   }
+
    return (
       <div className="min-h-screen mt-20">
          <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -14,33 +58,45 @@ const SignUp = () => {
             </div>
             {/* right */}
             <div className="flex-1">
-               <form className="flex flex-col gap-4">
+               <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                   <div>
                      <Label value="Your username" />
                      <TextInput
                         type="text"
                         placeholder="Username"
                         id="username"
+                        onChange={handleChange}
                      />
                   </div>
                   <div>
                      <Label value="Your email" />
                      <TextInput
-                        type="text"
+                        type="email"
                         placeholder="example@mail.ru"
-                        id="username"
+                        id="email"
+                        onChange={handleChange}
                      />
                   </div>
                   <div>
                      <Label value="Your password" />
                      <TextInput
-                        type="text"
+                        type="password"
                         placeholder="Password"
-                        id="username"
+                        id="password"
+                        onChange={handleChange}
                      />
                   </div>
-                  <Button gradientDuoTone={"purpleToBlue"} type="submit">
-                     Sign Up
+                  <Button gradientDuoTone={"purpleToBlue"} type="submit" disabled={loading}>
+                     {
+                        loading ? (
+                           <>
+                              <Spinner size="sm" />
+                              <span className="pl-3">Loading...</span>
+                           </>
+
+                        )
+                           : "Sign Up"
+                     }
                   </Button>
                </form>
                <div className="flex gap-2 text-sm mt-5">
@@ -49,6 +105,13 @@ const SignUp = () => {
                      Sign In
                   </Link>
                </div>
+               {
+                  errorMessage && (
+                     <Alert className="mt-5" color={"failure"}>
+                        {errorMessage}
+                     </Alert>
+                  )
+               }
             </div>
          </div>
       </div>
