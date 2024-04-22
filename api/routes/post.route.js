260881270b6cab49f872588c99cfce26994bb24a
dicreate <dkovchugo@mail.ru>
2024-04-22 22:46:32 +0300

    feat: create get posts route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { create } from "../conrollers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import { getPosts } from "../conrollers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+
+router.post("/getPosts", getPosts);
 
 export default router;
