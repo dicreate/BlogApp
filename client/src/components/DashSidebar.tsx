14dcccf738e8c279712790d03ca2cc8e72b0a43c
dicreate <dkovchugo@mail.ru>
2024-04-22 20:40:05 +0300

    feat: add posts section to the dashboard

--- client/src/components/DashSidebar.tsx
+++ client/src/components/DashSidebar.tsx
@@ -1,6 +1,6 @@
 import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiUser } from "react-icons/hi";
+import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 import useAuth from "../zustand/useAuth";
 
@@ -41,11 +41,11 @@ export default function DashSidebar() {
    return (
       <Sidebar className="w-full md:w-56">
          <Sidebar.Items>
-            <Sidebar.ItemGroup>
+            <Sidebar.ItemGroup className="flex flex-col gap-1">
                <Link to="/dashboard?tab=profile">
                   <Sidebar.Item
                      active={tab === 'profile'}
-                     icon={HiUser} label={currentUser?.isAdmin ? "admin" : "user"}
+                     icon={HiUser} label={currentUser?.isAdmin ? "Admin" : "User"}
                      labelColor="dark"
                      className='cursor-pointer'
                      as="div"
@@ -53,6 +53,16 @@ export default function DashSidebar() {
                      Profile
                   </Sidebar.Item>
                </Link>
+               {currentUser?.isAdmin && (<Link to="/dashboard?tab=posts">
+                  <Sidebar.Item
+                     active={tab === 'posts'}
+                     icon={HiDocumentText}
+                     className='cursor-pointer'
+                     as="div"
+                  >
+                     Posts
+                  </Sidebar.Item>
+               </Link>)}
 
                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
                   Sign Out
